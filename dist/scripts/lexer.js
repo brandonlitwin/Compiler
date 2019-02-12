019867b6270fae08d66056a845b08559e4b80d1b
Brandon Litwin <brandon.litwin1@marist.edu>
2019-02-11 20:51:23 -0500

    Started added some RegEx. The compiler can recognize whether the inputted tokens are valid

--- dist/scripts/lexer.js
+++ dist/scripts/lexer.js
@@ -6,7 +6,31 @@ var TSC;
         }
         Lexer.lex = function () {
             {
-                return tokens;
+                // Creating all the RegEx for the grammar
+                var allRegex = [];
+                allRegex = [new RegExp('{'),
+                    new RegExp('}'),
+                    new RegExp('[a-z]'),
+                    new RegExp('='),
+                    //new RegExp('$'),
+                    //new RegExp(' '),
+                    new RegExp('[0-9]')
+                ];
+                var foundArray = [];
+                var lextext = "Lexing program 1...\n";
+                for (var i = 0; i < tokens.length; i++) {
+                    var tokenFound = false;
+                    for (var j = 0; j < allRegex.length; j++) {
+                        if (allRegex[j].test(tokens.charAt(i))) {
+                            lextext += "Found Token " + tokens.charAt(i) + "\n";
+                            tokenFound = true;
+                        }
+                        if (j == allRegex.length - 1 && tokenFound == false) {
+                            lextext += "Error: Found Invalid Token " + tokens.charAt(i) + " \n";
+                        }
+                    }
+                }
+                return lextext;
             }
         };
         return Lexer;
--- source/scripts/lexer.ts
+++ source/scripts/lexer.ts
@@ -5,7 +5,35 @@ module TSC
 	export class Lexer {
 		public static lex() {
 		    {
-		        return tokens;
+				// Creating all the RegEx for the grammar
+				var allRegex = [];
+				allRegex = [new RegExp('{'), 
+							new RegExp('}'),
+							new RegExp('[a-z]'),
+							new RegExp('='),
+							//new RegExp('$'),
+							//new RegExp(' '),
+							new RegExp('[0-9]')
+				];
+				var foundArray = [];
+
+				var lextext = "Lexing program 1...\n";
+
+				for (var i = 0; i < tokens.length; i++) {
+					var tokenFound = false;
+					for (var j = 0; j < allRegex.length; j++) {
+						if (allRegex[j].test(tokens.charAt(i))) {
+							lextext += "Found Token " + tokens.charAt(i) + "\n";
+							tokenFound = true;
+						}
+						if (j == allRegex.length-1 && tokenFound == false) {
+							lextext += "Error: Found Invalid Token " + tokens.charAt(i) + " \n";
+						}
+						
+					}
+				}
+				
+				return lextext;
 				
 		    }
 		}
